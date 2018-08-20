488f308ead13c386b97cb8a28230d127d50bf197
Martin Gallegos <magallegosp@gmail.com>
2018-08-19 23:51:26 -0700

    Modifying styles and fonts for buttons and text

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  styles = {
+    fontSize: "50px",
+    fontWeight: "bold"
+  };
+
   render() {
-    return <h1>Hello Martin</h1>;
+    return (
+      <React.Fragment>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
