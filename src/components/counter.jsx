87d0efedcc772b7e500b1e9ddd3075b423e06c9f
Martin Gallegos <magallegosp@gmail.com>
2018-08-25 16:37:49 -0700

    Reverting tags...

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,28 +3,46 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: []
+    tags: ["tag1", "tag2", "tag3"]
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  styles = {
+    fontSize: "50px",
+    fontWeight: "bold"
+  };
 
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+  };
 
   render() {
     return (
-      <div>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
-      </div>
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
